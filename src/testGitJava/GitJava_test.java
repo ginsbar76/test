57db6884a0ab8e4d67a76bc7d5477cf929e32c80
macbooksadler <macbooksadler@macbook.home>
2017-05-22 22:49:37 +0200

    1ère version de Commit le 20170522

---
+++ src/testGitJava/GitJava_test.java
@@ -0,0 +1,14 @@
+package testGitJava;
+
+public class GitJava_test {
+
+	public GitJava_test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
